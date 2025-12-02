882f641caea3d51da6d8eabd28e0d136e446f0e4
Jorge Tome Blanco <jorgetomeb@gmail.com>
2025-12-02 17:47:51 +0100

    feat(146): Solution refined. Now it uses doubly linked list. LeetCode fails due Time Limit Exceeded.

--- Problems/146-LRU Cache.js
+++ Problems/146-LRU Cache.js
@@ -1,149 +1,183 @@
-class LinkedNode {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
+function LinkedNode(data) {
+    this.data = data;
+    this.next = null;
+    this.previous = null;
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-    }
+LinkedNode.prototype.resetNext = function() { this.next = null };
+LinkedNode.prototype.resetPrevious = function() { this.previous = null };
 
-    insertFirst(node) {
-        node.next = this.head;
-        this.head = node;
-        this.size++;
-        
-        if (this.size === 1)
-            this.tail = this.head;
-    }
+function LinkedList() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+}
 
-    insertLast(node) {
-        if (this.size) {
-            this.tail.next = node;
-        }
+LinkedList.prototype.push = function(node) {
+    // 1. Remove 'next' reference if it exists
+    node.resetNext();
+    // 2. Keep old tail node reference
+    const oldTail = this.tail;
+    if (oldTail) {
+        // 2.1 If old tail reference exists, old tail 'next' must point the new tail node and node's previous must point old tail
+        oldTail.next = node;
+        node.previous = oldTail;
         this.tail = node;
-        this.size++;
-
-        if (this.size === 1)
-            this.head = this.tail;
+    } else {
+        // 2.2 If old tail reference doesn't exist, node is the new tail & head
+        this.tail = node;
+        this.head = node;
+        node.resetPrevious();
     }
 
-    pop() {
-        const headNode = this.head;
+    // 3. Increment size
+    this.size++;
+}
 
-        if (this.head?.next) {
-            this.head = this.head.next;
-        } else if (this.head) {
-            this.head = null;
-            this.tail = null;
-        }
+LinkedList.prototype.remove = function(node) {
+    // 1. Remove the element disconnecting and reassigning required properties/variables
+    if (node.previous && node.next) {
+        // 1.1 If node has previous & next, previous node must point to the node's next reference and node's next's previous point to the node's previous.
+        const previous = node.previous;
+        const next = node.next;
+        previous.next = next;
+        next.previous = previous
+    } else if (node.previous) {
+        // 1.2 If node has previous but not next, it means node is in the tail,
+        // therefore node's previous must clean its next reference and become the new tail.
+        const previous = node.previous;
+        previous.next = null;
+        this.tail = previous;
+    } else if (node.next) {
+        // 1.3 If node has next, but not previous, it means node is in the head,
+        // therefore node's next must clean its previous reference and become the new head.
+        const next = node.next;
+        next.previous = null;
+        this.head = next;
+    } else if (this.tail === node && this.head === node) {
+        // 1.4 If node has neither next, nor previous it must be the last element or it's a disconnected element.
+        // In order to validate if it's the last element consider checking: head === node && tail === node.
+        this.tail = null;
+        this.head = null;
+    } else { this.size++ } // 1.5 Increment size just to put decrement statement unconditionally.
 
-        if (this.size > 0)
-            --this.size;
+    // 2. Reset node's properties to disconnect it from others
+    node.resetNext();
+    node.resetPrevious();
 
-        return headNode;
+    // 3. Decrement size
+    this.size--;
+}
+
+LinkedList.prototype.pop = function() {
+    const oldHead = this.head;
+    this.remove(this.head);
+    return oldHead;
+}
+
+LinkedList.prototype.print = function() {
+    let node = this.head;
+    let index = 0;
+    let text = '';
+    while (node) {
+        text += `[${index}] element = ${JSON.stringify(node.data)}\n`;
+        node = node.next;
+        index++; 
     }
+    console.log(text);
+}
 
-    print() {
-        let node = this.head;
-        let index = 0;
-        let text = '';
-        while (node) {
-            text += `[${index}] element = ${JSON.stringify(node.data)}\n`;
-            node = node.next;
-            index++; 
-        }
-        console.log(text);
+function LRUCache(capacity) {
+    if (capacity < 1 || capacity > 3000)
+        throw new Error(`Invalid capacity: ${capacity}`);
+    this.capacity = capacity;
+    this.hashMap = new Map();
+    this.linkedList = new LinkedList();
+}
+
+/**
+ * Get value related with the key if it exists, otherwise returns -1
+ * @param {number} key 
+ */
+LRUCache.prototype.get = function(key) {
+    if (!this.hashMap.has(key))
+        return -1
+
+    const { value } = this.hashMap.get(key);
+    this.put(key, value);
+    return value;
+}
+
+/**
+ * Modify value associated with the key if it exists, otherwise adds a new entry
+ * @param {number} key 
+ * @param {number} value 
+ */
+LRUCache.prototype.put = function(key, value) {
+    if (this.hashMap.has(key)) {
+        // Update node
+        this._update(key, value);
+    } else {
+        // Add node
+        this._add(key, value);
+        if (this.capacity < this.linkedList.size)
+            this._removeLeastRecent();
     }
 }
 
-class LRUCache {
-    constructor(capacity) {
-        if (capacity < 1 || capacity >= 3000)
-            throw new Error(`Invalid capacity: ${capacity}`);
-        this.capacity = capacity;
-        this.hashMap = new Map();
-        this.linkedList = new LinkedList();
-    }
-
-    /**
-     * Get value associated with the key if exists, otherwise returns -1
-     * @param {number} key 
-     */
-    get(key) {
-        if (!this.hashMap.has(key)) return -1
-        const { value } = this.hashMap.get(key);
-        this.put(key, value);
-        return value;
-    }
-
-    /**
-     * Modify value associated with the key if exists, otherwise adds a new entry
-     * @param {number} key 
-     * @param {number} value 
-     */
-    put(key, value) {
-        if (this.hashMap.has(key)) {
-            // Update node
-            let { previous } = this.hashMap.get(key);
-            const tail = this.linkedList.tail;
-            const nodeToUpdate = previous?.next ?? this.linkedList.head;
-            const nextOfNodeToUpdate = nodeToUpdate.next;
-            nodeToUpdate.next = null;
-            this.linkedList.insertLast(nodeToUpdate);
-            this.hashMap.set(key, { value, previous: tail });
-            
-            if (previous) {
-                // Update previous if exists
-                previous.next = nextOfNodeToUpdate;
-                const { value: nextValueOfNodeToUpdate } = this.hashMap.get(nextOfNodeToUpdate.data);
-                this.hashMap.set(nextOfNodeToUpdate.data, { value: nextValueOfNodeToUpdate, previous });
-            } else {
-                // Reassign Linked List HEAD node
-                this.linkedList.head = nextOfNodeToUpdate;
-            }
-        } else {
-            // Add new node
-            const previous = this.linkedList.tail;
-            const newNode = new LinkedNode(key);
-            this.hashMap.set(key, { value, previous });
-            this.linkedList.insertLast(newNode);
-
-            if (this.linkedList.size > this.capacity) {
-                const removedHead = this.linkedList.pop();
-                this.hashMap.delete(removedHead.data);
-
-                if (removedHead.next) {
-                    const nextOfRemovedHead = removedHead.next;
-                    const { value: nextValueOfNextOfRemovedHead } = this.hashMap.get(nextOfRemovedHead.data);
-                    this.hashMap.set(nextOfRemovedHead.data, { value: nextValueOfNextOfRemovedHead, previous: null });
-                }
-            }
-        }
-    }
+LRUCache.prototype._update = function(key, value) {
+    let { node } = this.hashMap.get(key);
+    this.linkedList.remove(node);
+    this.linkedList.push(node);
+    this.hashMap.set(key, { value, node: node });
 }
 
-const cache = new LRUCache(3);
-cache.put(1, 1);
-console.log('key 1, value 1 INSERTED');
-cache.linkedList.print();
-cache.put(2, 2);
-console.log('key 2, value 2 INSERTED');
-cache.linkedList.print();
-cache.put(3, 3);
-console.log('key 3, value 3 INSERTED');
-cache.linkedList.print();
-cache.put(2, 10);
-console.log('key 2, value 2 MODIFIED -> new value 10');
-cache.linkedList.print();
-console.log(cache.hashMap);
-console.log(cache.get(1));
-console.log('key:1 value OBTAINED');
-cache.linkedList.print();
-cache.put(4, 4);
-console.log('key 4, value 4 INSERTED');
-cache.linkedList.print();
+LRUCache.prototype._add = function(key, value) {
+    const newNode = new LinkedNode(key);
+    this.linkedList.push(newNode);
+    this.hashMap.set(key, { value, node: newNode });
+}
+
+LRUCache.prototype._removeLeastRecent = function() {
+    const oldHead = this.linkedList.pop();
+    this.hashMap.delete(oldHead.data);
+}
+
+
+const test_1 = function() {
+    const cache = new LRUCache(3);
+    cache.put(1, 1);
+    console.log('key 1, value 1 INSERTED');
+    cache.linkedList.print();
+    cache.put(2, 2);
+    console.log('key 2, value 2 INSERTED');
+    cache.linkedList.print();
+    cache.put(3, 3);
+    console.log('key 3, value 3 INSERTED');
+    cache.linkedList.print();
+    cache.put(2, 10);
+    console.log('key 2, value 2 MODIFIED -> new value 10');
+    cache.linkedList.print();
+    console.log(cache.hashMap);
+    console.log(cache.get(1));
+    console.log('key:1 value OBTAINED');
+    cache.linkedList.print();
+    cache.put(4, 4);
+    console.log('key 4, value 4 INSERTED');
+    cache.linkedList.print();
+}
+
+const test_2 = function() {
+    const cache = new LRUCache(2);
+    cache.put(1, 1);
+    cache.put(2, 2);
+    cache.get(1);
+    cache.put(3, 3);
+    console.log(`get(2) = ${cache.get(2)}`);
+    cache.put(4, 4);
+    cache.get(1);
+    cache.get(3);
+    cache.get(4);
+}
+
+test_2();
